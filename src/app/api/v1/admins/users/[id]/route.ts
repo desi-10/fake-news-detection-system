12661e6ca3dcac808c39915b6b5f4c02879ac932
PROBOOK 445R G7 <desmondkudjuh99@gmail.com>
2025-04-10 14:21:42 +0000

    update

--- src/app/api/v1/admins/users/[id]/route.ts
+++ src/app/api/v1/admins/users/[id]/route.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { prisma } from "@/utils/db";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
 export const GET = async (
   _request: Request,
@@ -28,6 +29,94 @@ export const GET = async (
     console.log("Error fetching user:", error);
     return NextResponse.json(
       { message: "Something went wrong!", error },
+      { status: 500 }
+    );
+  }
+};
+
+const updateUserSchema = z.object({
+  status: z.enum(["ACTIVATE", "DEACTIVATE"]),
+});
+
+export const PATCH = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const id = (await params).id;
+    const session = await auth();
+
+    if (!session || session.user.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = request.json();
+    const parsedBody = updateUserSchema.safeParse(body);
+    if (!parsedBody.success) {
+      return NextResponse.json({
+        error: "Invalid data",
+        errors: parsedBody.error.format(),
+      });
+    }
+
+    const { status } = parsedBody.data;
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        isAtive: status === "ACTIVATE",
+      },
+    });
+    return NextResponse.json(
+      { message: "User updated successfully", data: updatedUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error updating user:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELTE = async (
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const id = (await params).id;
+    const session = await auth();
+    if (!session || session.user.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 400 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    await prisma.user.update({
+      where: { id },
+      data: {
+        isAtive: false,
+      },
+    });
+  } catch (error) {
+    console.log("", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
       { status: 500 }
     );
   }
