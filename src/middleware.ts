40e5db5d1f3cb2876c002b18a0b2169f8d64c3b0
PROBOOK 445R G7 <desmondkudjuh99@gmail.com>
2025-04-10 16:15:35 +0000

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,26 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-// import { NextResponse } from "next/server";
 import { auth } from "./auth";
 import { NextResponse } from "next/server";
+import authConfig from "./auth.config";
 const { auth: middleware } = NextAuth(authConfig);
-
-// Allowed Roles
-const ALLOWED_ROLES = ["SUPER_ADMIN", "ADMIN", "SUB_ADMIN"];
 
 export default middleware(async (req) => {
   const session = await auth();
   const nextUrl = req.nextUrl;
 
-  if (
-    session?.user &&
-    ["/login", "/forgot-password", "/reset-password"].includes(nextUrl.pathname)
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (session?.user && nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/home", req.url));
   }
 
   if (
     nextUrl.pathname.startsWith("/dashboard") &&
-    !ALLOWED_ROLES.includes(session?.user?.role as string)
+    session?.user?.role !== "ADMIN"
   ) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (
-    ALLOWED_ROLES.includes(session?.user?.role as string) &&
-    nextUrl.pathname === "/"
-  ) {
-    return NextResponse.redirect(new URL("/dashboard/orders", req.url));
+  if (!session && nextUrl.pathname.startsWith("/home")) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 });
 
