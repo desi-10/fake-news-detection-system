f416b48a1be5386c228f81f9aa8a1a4a302a9b98
PROBOOK 445R G7 <desmondkudjuh99@gmail.com>
2025-05-11 08:31:38 +0000

    update

--- src/utils/extra-from-file.ts
+++ src/utils/extra-from-file.ts
@@ -1,6 +1,7 @@
-// import pdfParse from "pdf-parse";
+import pdfParse from "pdf-parse";
 import mammoth from "mammoth";
 import { createWorker } from "tesseract.js";
+
 export async function extractTextFromFile(file: Blob): Promise<string> {
   try {
     const arrayBuffer = await file.arrayBuffer();
@@ -8,24 +9,27 @@ export async function extractTextFromFile(file: Blob): Promise<string> {
     const fileType = file.type;
 
     switch (fileType) {
-      case "application/pdf":
-      // const pdfData = await pdfParse(buffer);
-      // return pdfData.text;
+      case "application/pdf": {
+        const pdfData = await pdfParse(buffer);
+        return pdfData.text;
+      }
 
       case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-      case "application/msword":
+      case "application/msword": {
         const { value } = await mammoth.extractRawText({ buffer });
         return value;
+      }
 
       case "image/png":
       case "image/jpeg":
-      case "image/jpg":
-        const worker = await createWorker();
+      case "image/jpg": {
+        const worker = await createWorker("eng");
         const {
           data: { text },
         } = await worker.recognize(buffer);
         await worker.terminate();
         return text;
+      }
 
       default:
         throw new Error(`Unsupported file type: ${fileType}`);
